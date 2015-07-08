cfc9b3ede51cb35981148cfd7efbd3a8ec26fc82
Alex Kilian <alexander.kilian@hhl.de>
2015-07-08 12:24:02 -0300

    added user auth, login page and restricted access

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
@@ -57,6 +59,7 @@ def category(request, category_name_slug):
     return render(request, 'rango/category.html', context_dict)
 
 
+@login_required
 def add_category(request):
     # A HTTP POST?
     if request.method == 'POST':
@@ -80,6 +83,7 @@ def add_category(request):
     return render(request, 'rango/add_category.html', {'form': form})
 
 
+@login_required
 def add_page(request, category_name_slug):
 
     try:
@@ -134,7 +138,7 @@ def register(request):
             # go ahead and save user form data into db
             user = user_form.save()
 
-            # hash password wit seg_password method
+            # hash password wit set_password method
             # once hashed update the user object
             user.set_password(user.password)
             user.save()
@@ -154,7 +158,7 @@ def register(request):
 
             # update registered variable
             registered = True
-            return HttpResponse("Temporary success!")
+            # return HttpResponse("Temporary success!")
 
         # print out errors if form contain missing values or errors
         else:
@@ -172,3 +176,61 @@ def register(request):
                 {'user_form': user_form,
                 'profile_form': profile_form,
                 'registered': registered})
+
+
+def user_login(request):
+    # If the request is a HTTP POST,
+    # try to pull out the relevant information.
+    if request.method == 'POST':
+        # Gather the username and password provided by the user.
+        # This information is obtained from the login form.
+            # We use request.POST.get('<variable>') as opposed to request.POST['<variable>'],
+            # because request.POST.get('<variable>') returns None, if the value does not exist,
+            # while request.POST['<variable>'] will raise key error exception
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # Use Django's machinery to attempt to see if the username/password
+        # combination is valid - a User object is returned if it is.
+
+        user = authenticate(username=username, password=password)
+
+        # If we have a User object, the details are correct.
+        # If None (Python's way of representing the absence of a value)
+        # no user with matching credentials was found.
+
+        if user:
+            # is account active
+            if user.is_active:
+                # If the account is valid and active, we can log the user in.
+                # We'll send the user back to the homepage.
+                login(request, user)
+                return HttpResponseRedirect('/rango/')
+            else:
+                # An inactive account was used - no logging in!
+                return HttpResponse("your Rango account is disabled.")
+        else:
+            # Bad login details were provided. So we can't log the user in.
+            # print "Invalid login details: {0}, {1}.".format(username, password)
+            return HttpResponse("Invalid login details: {0}, {1}.".format(username, password))
+
+    # The reques is not a HTTP POST, so display the login form.
+    # this scenario would most likely be a HTTP Get
+    else:
+        # No context variables to pass to the template system, hence the
+        # blank dictionary object...
+        return render(request, 'rango/login.html', {})
+
+
+@login_required
+def restricted(request):
+    return HttpResponse("Since you're logged in , you can see this text!")
+
+
+@login_required
+def user_logout(request):
+    # Since we know the user is logged in, we can now just log them out.
+    logout(request)
+
+    # Take the user back to the homepage.
+    return HttpResponseRedirect('/rango/')
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -15,6 +15,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
 STATIC_PATH = os.path.join(BASE_DIR, 'static')
 
+LOGIN_URL = '/rango/login/'
+
 
 # Printing paths for sanity's sake
 print "Templates:", TEMPLATE_PATH
