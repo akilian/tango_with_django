3471b137aee373a60d9b808a766c3e8107247ed0
Alex Kilian <alexander.kilian@hhl.de>
2015-07-13 10:26:06 -0300

    added jquery functionality

--- static/js/rango-jquery.js
+++ static/js/rango-jquery.js
@@ -4,4 +4,17 @@ $(document).ready( function(){
 		alert("You clicked the button using JQuery!")
 	});
 
+	$("p").hover(function(){
+		$(p).css('color', 'red');
+	},
+	function(){
+		$(p).css('color', 'blue');
+	});
+
+	$("#about-btn").click( function(event){
+	msgstr = $("#msg").html()
+		msgstr = msgstr + "o"
+		$("#msg").html(msgstr)
+	});
+
 });
