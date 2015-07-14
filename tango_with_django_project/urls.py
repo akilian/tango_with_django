43399173e9961594686b81a42d26353ca567fdfa
Alex Kilian <alexander.kilian@hhl.de>
2015-07-14 16:01:40 -0300

    added url root config

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -15,7 +15,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'tango_with_django_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', include('rango.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^rango/', include('rango.urls')),
     url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
